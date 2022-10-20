a2115e59f259f92aa7bb9522b587e739d914c08a
Finn Rudolph <finn.rudolph@t-online.de>
2022-10-20 17:13:01 +0200

    Implement the FFT

--- convolution.c
+++ convolution.c
@@ -1,5 +1,12 @@
+#include <complex.h>
+#include <math.h>
+
 #include "convolution.h"
 #include "util.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 void convolve(
     size_t n, size_t k, double *const *const in,
@@ -44,4 +51,49 @@ void convolve_pad(
             }
         }
     }
+}
+
+void bit_reverse(
+    size_t n, size_t lgn, double *const vec, complex double *const out)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        size_t rev_i = 0;
+        for (size_t j = 0; j < lgn; j++)
+        {
+            rev_i |= (i & (1 << j) ? 1 : 0) << (lgn - j - 1);
+        }
+        out[rev_i] = vec[i];
+    }
+}
+
+// Returns the Discrete Fourier Transform of vec, where n must be a power of 2.
+complex double *fft(size_t n, double *const vec)
+{
+    size_t lgn = 0;
+    while (1 << lgn < n)
+        lgn++;
+
+    complex double *res = malloc(n * sizeof(complex double));
+    bit_reverse(n, lgn, vec, res);
+
+    for (size_t s = 1; s <= lgn; s++)
+    {
+        size_t l = 1 << s;
+        complex double omega_m = exp(2.0 * M_PI * I / (complex double)l);
+
+        for (size_t i = 0; i < n; i += s)
+        {
+            complex double omega = 1.0;
+            for (size_t j = i; j < i + l / 2; j++)
+            {
+                complex double u = res[j], v = omega * res[j + l / 2];
+                res[j] = u + v;
+                res[j + l / 2] = u - v;
+                omega *= omega_m;
+            }
+        }
+    }
+
+    return res;
 }
